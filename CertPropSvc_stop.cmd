548a4f9afc50be61de5ee696b60500477633823c
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2022-05-30 00:01:36 +0300

    Update CertPropSvc_stop.cmd
    Just a misspelling correction

--- CertPropSvc_stop.cmd
+++ CertPropSvc_stop.cmd
@@ -3,10 +3,10 @@ cls
 
 echo.
 echo.
-echo Stopping CertPropSvc (Certification Propagation Service): 
+echo Stopping CertPropSvc (Certificate Propagation Service): 
 
 sc config CertPropSvc start=disabled
 sc stop CertPropSvc
 
-echo CertPropSvc (Certification Propagation Service) has been stopped successfully!
+echo CertPropSvc (Certificate Propagation Service) has been stopped successfully!
 pause
